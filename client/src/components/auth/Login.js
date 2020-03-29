ffc8a8ad532f46945e3437614cfc6623563ea056
vinhl <vinhl@DESKTOP-P80T53I>
2020-03-29 12:02:36 +0700

    init front-end

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,69 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
+  return (
+    <Fragment>
+      {/* Alert */}
+      <div className='alert alert-danger'>Invalid Credentials</div>
+
+      <a href='dashboard.html' className='my-1'>
+        <i className='fas fa-code fa-4x'></i>
+      </a>
+      <p className='text-light font-weight-normal my-1'>
+        Sign in to DevVitality
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            name='email'
+            placeholder='Email Address'
+            onChange={e => onChange(e)}
+            value={email}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            onChange={e => onChange(e)}
+            value={password}
+            minLength='6'
+          />
+        </div>
+
+        <input
+          type='submit'
+          value='Sign In'
+          className='btn btn-primary btn-block btn-large'
+        />
+      </form>
+      <p className='my-1'>
+        New to DevVitality?
+        <Link to='/register'>Create an account</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,92 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      console.log("SUCCESS");
+    }
+  };
+
+  return (
+    <Fragment>
+      <small className='form-text my-1'>Join DevVitality</small>
+      <h2 className='large text-primary sm-hide'>Create Your Account</h2>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <small className='form-text'>
+            This site uses Gravatar, so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={password}
+            required
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            minLength='6'
+            value={password2}
+            required
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block btn-large'
+        />
+      </form>
+      <p className='my-1'>
+        Already have an account?
+        <Link to='/login'>Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Register;
